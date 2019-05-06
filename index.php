6c51090202795937a7c6a4e1104a6778d679983b
Mark Paspirgilis <external_github@power-of-m.de>
2019-05-06 10:01:30 +0200

    modes

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include 'xpm/loader.php';
-
-var_dump(XPM_FILE_config);
-var_dump(XPM::$project);
--- xpm/loader.php
+++ xpm/loader.php
@@ -3,4 +3,5 @@
 include_once str_replace('\\', '/', __DIR__ . '/connect.php');
 if (class_exists('XPM')) {
     XPM::init(false);
+    XPM::load();
 }
